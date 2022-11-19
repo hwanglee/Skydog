85034eca73586918f7fad2958a57f0137a4b63a0
Hwang Lee <hwangseung@gmail.com>
2022-11-19 01:23:50 -0500

    create data loader tests

---
+++ SkydogTests/DataLoaderTests.swift
@@ -0,0 +1,45 @@
+//
+//  DataLoaderTests.swift
+//  SkydogTests
+//
+//  Created by Hwang Lee on 11/19/22.
+//
+
+import XCTest
+@testable import Skydog
+
+final class DataLoaderTests: XCTestCase {
+    let artistSlug = "grateful-dead"
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testFetchArtists() async {
+        let artists = (try? await DataLoader.shared.fetchArtists()) ?? []
+        
+        XCTAssertTrue(!artists.isEmpty)
+    }
+
+    func testFetchYears() async {
+        let years = (try? await DataLoader.shared.fetchYears(artistSlug: artistSlug)) ?? []
+        
+        XCTAssertTrue(!years.isEmpty)
+    }
+    
+    func testFetchShows() async {
+        let shows = (try? await DataLoader.shared.fetchShows(artistSlug: artistSlug)) ?? []
+        
+        XCTAssertTrue(!shows.isEmpty)
+    }
+    
+    func testFetchRecentShows() async {
+        let shows = (try? await DataLoader.shared.fetchRecentShows(artistSlug: artistSlug)) ?? []
+        
+        XCTAssertTrue(!shows.isEmpty)
+    }
+}
